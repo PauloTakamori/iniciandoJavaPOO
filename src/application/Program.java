4ba59fd90b2c55e094b4353db304d14659f5cbef
PauloTakamori <lino.x@hotmail.com>
2022-06-10 08:50:05 -0300

    Atualizando aquivo no GIT primeria vez

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,8 +4,9 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Program {
-
+	
 	public static void main(String[] args) {
+		//modificando aqui no git
 		Locale.setDefault(Locale.US);
 		Scanner teclado = new Scanner(System.in);
 		
@@ -21,10 +22,10 @@ public class Program {
 		yB = teclado.nextDouble();
 		yC = teclado.nextDouble();
 		
-		double p = (xA + xB + xC) / 2.0;
+		double p = (xA + xB + xC) / 2.00;
 		double areaX = Math.sqrt(p * (p - xA) * (p - xB) - (p - xC));
 		
-		p = (yA + yB + yC) / 2.0;
+		p = (yA + yB + yC) / 2.00;
 		double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
 		
 		System.out.printf("Triangle X area: %.4f%n", areaX);
